16159a118176db316494c88ae3f75583e91627c5
neilk17 <kanakianeil@gmail.com>
2023-05-03 12:24:08 +0100

    rename lazyvim to nvim

--- LazyVim/lua/plugins/fugitive.lua
+++
@@ -1,4 +0,0 @@
-return {
-  -- add fugitive
-  { "tpope/vim-fugitive", keys = { "<leader>gs", "<cmd>Git<cr>", desc = "Git status" } },
-}
--- nvim/lua/neilk17/plugins/colorscheme.lua
+++
@@ -1,6 +0,0 @@
-return {
-    -- Kanagawa
-    {
-        "rebelot/kanagawa.nvim"
-    }
-}
