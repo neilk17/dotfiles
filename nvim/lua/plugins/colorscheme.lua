0a0c46800697eceda66e2d351a1727d887fa264d
neilk17 <kanakianeil@gmail.com>
2023-05-11 11:54:34 +0100

    themes, linting and formatting

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   { "rebelot/kanagawa.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "oxocarbon",
     },
   },
 }
