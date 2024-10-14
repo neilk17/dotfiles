938784115e0638a19a5ad4ec1b53af79cdbec6dd
neilk17 <kanakianeil@gmail.com>
2024-10-15 00:03:39 +0530

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,39 +21,20 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Setup lazy.nvim
--- require("lazy").setup({
---   spec = {
---     { import = "plugins" },
---   },
---   -- Configure any other settings here. See the documentation for more details.
---   -- colorscheme that will be used when installing plugins.
---   install = { colorscheme = { "habamax" } },
---   -- automatically check for plugin updates
---   checker = { enabled = true },
--- })
-
 require("lazy").setup({
-	spec = {
-		{
-			"folke/zen-mode.nvim",
-			config = function()
-				require("zen-mode").setup({
-					window = {
-						width = 90,
-						options = {
-							number = false,
-							relativenumber = false,
-						},
+	"folke/which-key.nvim",
+	"folke/neoconf.nvim",
+	{
+		"folke/zen-mode.nvim",
+    opts={
+				window = {
+					width = 90,
+					options = {
+						number = false,
+						relativenumber = false,
 					},
-				})
-				-- Key mapping to toggle Zen Mode
-				vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", { silent = true })
-			end,
-		},
+				},
+    }
 	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
