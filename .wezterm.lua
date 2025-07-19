2b56b980a559938ed2fc125a1915486173b1356f
neilk17 <kanakianeil@gmail.com>
2025-07-19 15:08:58 +0800

    updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,10 +15,15 @@ config.colors = {
 }
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 19
-config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 20
 
 config.window_decorations = "RESIZE"
--- config.window_background_opacity = 0.90
-config.macos_window_background_blur = 8
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 20
 
 return config
